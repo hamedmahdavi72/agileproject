bb4d6bffd3ed3c58020b2cc0d8c40d48b053dce3
AlaEmrani <emranyala@gmail.com>
2017-01-08 15:19:01 +0330

    Merge branch 'localbranch' of /Users/hamed/Documents/course/IdeaProjects/agileproject with conflicts.

--- app/controllers/DoctorsHandler.java
+++ app/controllers/DoctorsHandler.java
@@ -1,12 +1,18 @@
 package controllers;
 
+import dao.AppointmentDAOWrapper;
 import dao.DoctorDAOWrapper;
 import forms.DoctorInfoForm;
+import models.Appointment;
 import models.User;
+import org.jongo.MongoCursor;
 import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
+import play.mvc.Security;
 import play.twirl.api.Content;
+
+import java.util.Date;
 
 /**
  * Created by HamedMahdavi on 1/6/2017.
@@ -24,5 +30,18 @@ public class DoctorsHandler extends Controller {
     public static Result getDoctorInfo(String username) {
         DoctorInfoForm doctorInfo = new DoctorInfoForm(DoctorDAOWrapper.getInstance().findByUsername(username));
         return ok(Json.toJson(doctorInfo));
+    }
+
+    @Security.Authenticated(Secured.class)
+    public static Result getDoctorAppointments(String username){
+        if(User.isDoctor(username)){
+
+            Date dateobj = new Date();
+            MongoCursor<Appointment> appointments = AppointmentDAOWrapper.getInstance().
+                    getAppointmentsAfterSpecificDate(username,dateobj);
+            return ok(Json.toJson(appointments));
+        }
+        else
+            return null;
     }
 }
--- app/dao/AppointmentDAOWrapper.java
+++ app/dao/AppointmentDAOWrapper.java
@@ -12,8 +12,6 @@ import java.util.Date;
 public class AppointmentDAOWrapper {
     private GenericDAO<Appointment> appointmentDAO = null;
     private static AppointmentDAOWrapper instance = new AppointmentDAOWrapper();
-    private final String FIND_AFTER_SPECIFIC_DATE_QUERY = "{$and:[{appointmentDate: {$gte: #}},{doctorUsername: #}]}";
-
 
     private AppointmentDAOWrapper(){
         this.appointmentDAO = new GenericDAO<>(Appointment.class);
@@ -23,21 +21,13 @@ public class AppointmentDAOWrapper {
         return instance;
     }
 
-
-
-    public MongoCursor<Appointment> findByCustomerUsername(String username){
-        return appointmentDAO.findByFieldName("customerUsername",username);
+    public Appointment findById(ObjectId id){
+        return appointmentDAO.findOneByFieldName("_id",id.toString());
     }
-
-    public MongoCursor<Appointment> findByDoctorUsername(String username){
-        return appointmentDAO.findByFieldName("doctorUsername",username);
-    }
-
-
 
     public MongoCursor<Appointment> getAppointmentsAfterSpecificDate(String doctorUsername, Date date){
-        return appointmentDAO.getCollection().find(FIND_AFTER_SPECIFIC_DATE_QUERY,date,doctorUsername)
-                .as(Appointment.class);
+
+        return null;
     }
 
 
